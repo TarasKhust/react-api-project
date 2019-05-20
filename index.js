7cbef128a49105197cb62217e2145e5a9dc67d36
Taras Rushchak <taraskhust@gmail.com>
2019-05-20 16:40:55 +0300

    add toogle class
    Signed-off-by: Taras Rushchak <taraskhust@gmail.com>

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from 'react-dom';
-import App from './src/App';
-import './src/api'
-import 'babel-polyfill';
-
-ReactDOM.render(<App/>, document.querySelector('#root')); /*?*/
--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,48 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './item-list.css';
+import "./item-list.css";
 
-import SwapiService from '../../services/swapi-service';
-import Spinner from '../spinner/spinner';
+import SwapiService from "../../services/swapi-service";
+import Spinner from "../spinner/spinner";
 
 export default class ItemList extends Component {
-	swapiService = new SwapiService();
+  swapiService = new SwapiService();
 
-	state = {
-		peopleList: null,
-	};
+  state = {
+    peopleList: null,
+    active: "active"
+  };
 
-	componentDidMount() {
-		this.swapiService.getAllPeople().then(( peopleList ) => {
-			this.setState({ peopleList });
-		});
-	}
+  componentDidMount() {
+    this.swapiService.getAllPeople().then(peopleList => {
+      this.setState({ peopleList });
+    });
+  }
 
-	renderItemes( arr ) {
-		return arr.map(( {id, name} ) => {
-			return (
-				<li className="list-group-item"
-				    key={id}
-				    onClick={() => this.props.onItemSelected(id)}>
-					{name}
-				</li>
-			);
-		});
-	}
+  renderItemes(arr) {
+    return arr.map(({ id, name }) => {
+      return (
+        <li
+          className="list-group-item 1"
+          key={id}
+          onClick={() => this.props.onItemSelected(id)}
+        >
+          {name}
+        </li>
+      );
+    });
+  }
 
-	render() {
-		const { peopleList } = this.state;
-		console.log(peopleList);
+  render() {
+    const { peopleList } = this.state;
 
-		if (!peopleList) {
-			return <Spinner/>;
-		}
+    if (!peopleList) {
+      return <Spinner />;
+    }
 
-		const items = this.renderItemes(peopleList);
-		return (
-			<ul className="item-list list-group">
-				{items}
-			</ul>
-		);
-	}
+    const items = this.renderItemes(peopleList);
+    return (
+      <ul
+        className="item-list list-group"
+        onClick={evt => {
+          const target = evt.target;
+          console.log(target);
+	        if (target.classList.contains('1')) {
+		        target.classList.remove('1');
+		        target.classList.add('active');
+	        } else if (target.classList.contains('active')) {
+	        	target.classList.remove('active');
+		        target.classList.add('1');
+	        }
+        }}
+      >
+        {items}
+      </ul>
+    );
+  }
 }
