0a1c2d174bcd46800e0b0ae9fc25daf3d739897d
Taras Rushchak <taraskhust@gmail.com>
2019-06-13 11:59:50 +0300

    add navLink

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import './header.css';
 
@@ -12,21 +12,11 @@ const Header = ({ onServiceChange }) => {
         </Link>
       </h3>
       <ul className="d-flex">
-        <li>
-          <Link to="/people/">People</Link>
-        </li>
-        <li>
-          <Link to="/planets/">Planets</Link>
-        </li>
-        <li>
-          <Link to="/starships/">Starships</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/secret">Secret</Link>
-        </li>
+          <li><NavLink activeClassName='active-link' to="/people/">People</NavLink></li>
+          <li><NavLink activeClassName='active-link' to="/planets/">Planets</NavLink></li>
+          <li><NavLink activeClassName='active-link' to="/starships/">Starships</NavLink></li>
+          <li><NavLink activeClassName='active-link' to="/login">Login</NavLink></li>
+          <li><NavLink activeClassName='active-link' to="/secret">Secret</NavLink></li>
       </ul>
 
       <button
